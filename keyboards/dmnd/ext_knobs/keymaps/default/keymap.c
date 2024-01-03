3979706a027fc3eb841583426523139a277ff04d
dmytro.popov <podotris@gmail.com>
2024-01-03 23:06:10 +0200

    updated encoder support; added second layer

--- keyboards/dmnd/ext_knobs/keymaps/default/keymap.c
+++ keyboards/dmnd/ext_knobs/keymaps/default/keymap.c
@@ -2,12 +2,16 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT(
-    KC_MUTE
+    LT(1,KC_MUTE)
+  ),
+  [1] = LAYOUT(
+    _______
   )
 };
 
 #ifdef ENCODER_MAP_ENABLE
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
   [0] = { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+  [1] = { ENCODER_CCW_CW(KC_WH_D, KC_WH_U) },
 };
 #endif
